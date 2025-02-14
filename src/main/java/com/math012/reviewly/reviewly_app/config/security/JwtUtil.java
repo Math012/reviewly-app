fa454904fa49f90a820844e03fd516339717c4e5
Math012 <math111.0811@gmail.com>
2025-02-14 01:41:35 -0300

    Criando a classe responsável pela criação do token JWT.

---
+++ src/main/java/com/math012/reviewly/reviewly_app/config/security/JwtUtil.java
@@ -0,0 +1,49 @@
+package com.math012.reviewly.reviewly_app.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class JwtUtil {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private  String keyToken = "secret";
+
+    private long validationTime = 18000000;
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + validationTime))
+                .signWith(Keys.hmacShaKeyFor(keyToken.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(keyToken.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String username){
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username)&& !isTokenExpired(token));
+    }
+
+}
