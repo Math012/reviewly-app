a4157d76470cf1bd6c51c9de4493e094cad20e22
Math012 <math111.0811@gmail.com>
2025-02-14 01:03:00 -0300

    Classe review criada e implementada

---
+++ src/main/java/com/math012/reviewly/reviewly_app/domain/model/Review.java
@@ -0,0 +1,33 @@
+package com.math012.reviewly.reviewly_app.domain.model;
+
+import com.math012.reviewly.reviewly_app.enums.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "tb_title")
+    private String titleOfTheWork;
+    // TODO Place validation dependence on the defined limiter for rating (0, 5)
+    private Integer rating;
+    @Column(name = "tb_review")
+    private String review;
+    @Column(name = "tb_date")
+    private LocalDate date;
+    @Column(name = "tb_category")
+    private Category category;
+}
