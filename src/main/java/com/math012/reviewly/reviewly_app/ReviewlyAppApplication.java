c03b3b4f15e8f75201fe03448995af04b2412ef5
Math012 <math111.0811@gmail.com>
2025-02-14 00:18:55 -0300

    Initial commit

---
+++ src/main/java/com/math012/reviewly/reviewly_app/ReviewlyAppApplication.java
@@ -0,0 +1,13 @@
+package com.math012.reviewly.reviewly_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewlyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewlyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/reviewly/reviewly_app/ReviewlyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.reviewly.reviewly_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewlyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
