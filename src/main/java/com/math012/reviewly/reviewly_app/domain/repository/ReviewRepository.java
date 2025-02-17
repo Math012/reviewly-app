9dbbcdd1a8728f08e25cb6cdc3b31feb67a6a31a
Math012 <math111.0811@gmail.com>
2025-02-17 02:36:14 -0300

    Implementando o ReviewRepository com as Querys personalizadas: findAllCategory e findAllReviewsByUsername

--- src/main/java/com/math012/reviewly/reviewly_app/domain/repository/ReviewRepository.java
+++ src/main/java/com/math012/reviewly/reviewly_app/domain/repository/ReviewRepository.java
@@ -1,9 +1,23 @@
 package com.math012.reviewly.reviewly_app.domain.repository;
 
 import com.math012.reviewly.reviewly_app.domain.model.Review;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT c FROM Review c WHERE c.category=:categorySelected")
+    List<Object> findAllCategory(@Param("categorySelected")String categorySelected);
+
+    @Query("SELECT r FROM Review r WHERE r.user.username =:username")
+    List<Review> findAllReviewsByUsername(@Param("username") String username);
+
+
 }
