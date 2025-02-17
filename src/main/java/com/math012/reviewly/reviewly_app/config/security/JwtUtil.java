ea205610cadd737bbb02068e08e36fb2e45ba50a
Math012 <math111.0811@gmail.com>
2025-02-17 02:28:32 -0300

    Adicionando a anotação service ao JwtUtil para que o sistema de injeção de dependência do Spring funcione

--- src/main/java/com/math012/reviewly/reviewly_app/config/security/JwtUtil.java
+++ src/main/java/com/math012/reviewly/reviewly_app/config/security/JwtUtil.java
@@ -5,10 +5,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+@Service
 public class JwtUtil {
 
     @Value("${security.jwt.token.secret-key:secret}")
