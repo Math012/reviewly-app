16e1891289a1330a0d213c0fe8284e1ddb1ea0e2
Math012 <math111.0811@gmail.com>
2025-02-17 02:33:27 -0300

    Criando as classes DTOs para a transferência de dados entre objetos

---
+++ src/main/java/com/math012/reviewly/reviewly_app/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.math012.reviewly.reviewly_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserDTO {
+    private String username;
+    private String name;
+    private String lastname;
+}
